fd3be9639c2ad8b51237e7e7ccf5e8b2271c9c59
Nezar Alazzabi <87538486+COD0xb0@users.noreply.github.com>
2021-07-16 17:08:31 +0100

    Add files via upload

---
+++ OneDrive_Enum/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿
+namespace OneDrive_Enum
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.UserID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtext = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtdomain = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(166, 163);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(390, 27);
+            this.txtusername.TabIndex = 2;
+            this.txtusername.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtusername.TextChanged += new System.EventHandler(this.txtusername_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(169)))), ((int)(((byte)(244)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(166, 207);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Check Single Username";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(247)))));
+            this.label3.Location = new System.Drawing.Point(319, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "username";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 282);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(706, 496);
+            this.textBox1.TabIndex = 7;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserID});
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.Location = new System.Drawing.Point(724, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dataGridView1.RowHeadersWidth = 51;
+            dataGridViewCellStyle5.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(471, 766);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // UserID
+            // 
+            this.UserID.HeaderText = "Employee ID";
+            this.UserID.MinimumWidth = 6;
+            this.UserID.Name = "UserID";
+            this.UserID.ReadOnly = true;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(169)))), ((int)(((byte)(244)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(366, 207);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Check Users List";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(247)))));
+            this.label1.Location = new System.Drawing.Point(12, 251);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(234, 28);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ENUMERATION RESULT";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(3)))), ((int)(((byte)(169)))), ((int)(((byte)(244)))));
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(527, 207);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(31, 29);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "...";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(247)))));
+            this.label2.Location = new System.Drawing.Point(339, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 20);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Ext";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtext
+            // 
+            this.txtext.Location = new System.Drawing.Point(168, 96);
+            this.txtext.Name = "txtext";
+            this.txtext.Size = new System.Drawing.Size(390, 27);
+            this.txtext.TabIndex = 13;
+            this.txtext.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtext.TextChanged += new System.EventHandler(this.txtext_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(247)))));
+            this.label4.Location = new System.Drawing.Point(325, 8);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 20);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Domain";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // txtdomain
+            // 
+            this.txtdomain.Location = new System.Drawing.Point(166, 31);
+            this.txtdomain.Name = "txtdomain";
+            this.txtdomain.Size = new System.Drawing.Size(390, 27);
+            this.txtdomain.TabIndex = 15;
+            this.txtdomain.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.txtdomain.TextChanged += new System.EventHandler(this.txtdomain_TextChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(71)))), ((int)(((byte)(79)))));
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 784);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1207, 58);
+            this.panel1.TabIndex = 17;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(366, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(508, 40);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "One_Enum is a tool to enumerate valid OneDrive users in your corporation...\r\nThis" +
+    " tool for educational purposes only.";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.ClientSize = new System.Drawing.Size(1207, 842);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtdomain);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtext);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtusername);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "One_Enum";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserID;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtext;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtdomain;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ OneDrive_Enum/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+
+namespace OneDrive_Enum
+{
+
+    public partial class Form1 : Form
+    {
+        public string domainname;
+        public string extension;
+        public string userslist;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               var username = txtusername.Text.ToString();
+                string URL = @"https://'" + txtdomain.Text + "'-my.sharepoint.com/personal/'" + txtusername.Text + "'_'" + txtext.Text + "'/_layouts/15/onedrive.aspx";
+                URL = URL.Replace("'", "");
+                var request = WebRequest.Create(URL);
+                request.Method = "GET";
+                using var webResponse = request.GetResponse();
+                using var webStream = webResponse.GetResponseStream();
+                using var reader = new StreamReader(webStream);
+                var data = reader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                if (ex.Message.Contains("404"))
+                {
+                    textBox1.Text = "USER " + txtusername.Text + " IS NOT VALID";
+                }
+                else
+                {
+                    textBox1.Text = "USER " + txtusername.Text + " IS VALID";
+                }
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    try
+                    {
+                        var username = row.Cells[0].Value.ToString();
+                        string URL = @"https://'" + txtdomain.Text + "'-my.sharepoint.com/personal/'" + username + "'_'" + txtext.Text + "'/_layouts/15/onedrive.aspx";
+                        URL = URL.Replace("'", "");
+                        var request = WebRequest.Create(URL);
+                        request.Method = "GET";
+                        using var webResponse = request.GetResponse();
+                        using var webStream = webResponse.GetResponseStream();
+                        using var reader = new StreamReader(webStream);
+                        var data = reader.ReadToEnd();
+                    }
+                    catch (Exception ex)
+                    {
+
+                        if (ex.Message.Contains("403"))
+                        {
+                        textBox1.Text = textBox1.Text + " User: " + row.Cells[0].Value.ToString() + " IS VALID" + System.Environment.NewLine ;
+                        }
+                   else
+                    {
+                        textBox1.Text = textBox1.Text + " User: " + row.Cells[0].Value.ToString() + " IS NOT VALID" + System.Environment.NewLine;
+
+                    }
+                }
+                   
+                }
+            }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text Files|*.txt;";
+            ofd.ShowDialog();
+            ofd.OpenFile();
+            userslist = ofd.FileName.ToString();
+            Load_Emp_IDS(userslist);
+        }
+
+
+        private void Load_Emp_IDS(String filename)
+        {
+            // Declare a string variabel
+            String sLine = "";
+
+            //Clear All selections in DGV
+            dataGridView1.ClearSelection();
+
+            try
+            {
+                dataGridView1.Rows.Clear();
+                // Stream Reader to read branches data from the text file      
+                System.IO.StreamReader FileStream = new System.IO.StreamReader(filename);
+
+                // Pervent users from creating a new row in the DGV
+                dataGridView1.AllowUserToAddRows = false;
+
+                // Set Sline string var reads lines in the branches text
+                sLine = FileStream.ReadLine();
+
+                // split lines by ;
+                string[] s = sLine.Split(';');
+
+                // Read text file content and add it to DGV
+                sLine = FileStream.ReadLine();
+
+                // while still reading and there's lines 
+                while (sLine != null)
+                {
+                    // add lines as rows in DGV
+                    dataGridView1.Rows.Add();
+
+
+                    for (int i = 0; i <= s.Count() - 1; i++)
+                    {
+                        s = sLine.Split(';');
+                        dataGridView1.Rows[dataGridView1.Rows.Count - 1].Cells[0].Value = s[0].ToString();
+                    }
+                    sLine = FileStream.ReadLine();
+                }
+
+                // Close the file Steam ( Stop Reading )
+                FileStream.Close();
+
+            }
+            catch (Exception err)
+            {
+                // show message if there's an error
+                System.Windows.Forms.MessageBox.Show(err.Message);
+            }
+        }
+
+        private string ReplaceQuotes(string sentence)
+
+        {
+
+            return sentence.Replace("\"", "").Replace("'", "");
+
+        }
+
+
+    }
+}
+        
+
+       
